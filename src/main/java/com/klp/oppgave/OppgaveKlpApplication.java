df4b8d8407615a8014494ee08736b313c21d8977
CharlesSE <charles.se@live.no>
2025-04-04 19:59:19 +0200

    Initiell commit Spring Boot-applikasjon

---
+++ src/main/java/com/klp/oppgave/OppgaveKlpApplication.java
@@ -0,0 +1,12 @@
+package com.klp.oppgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppgaveKlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppgaveKlpApplication.class, args);
+	}
+}
