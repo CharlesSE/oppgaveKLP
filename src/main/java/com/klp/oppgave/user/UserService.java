f6161e852e0d9606c4fdb3db9c18ed07ead6c70e
CharlesSE <charles.se@live.no>
2025-04-08 22:53:56 +0200

    Fjernet noe unødvendig kode

--- src/main/java/com/klp/oppgave/user/UserService.java
+++ src/main/java/com/klp/oppgave/user/UserService.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,9 +15,6 @@ public class UserService {
     }
 
     public User createUser(User user) {
-        if (user.getEmail() == null || user.getType() == null) {
-            throw new IllegalArgumentException("Email and type are required");
-        }
         return userRepository.save(user);
     }
 
